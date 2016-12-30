4b1766d5826af87e0d9db7d73f9c5c973ad71fe5
xi-studio <110littletree@sina.com>
2016-12-30 11:50:04 +0800

    fix

--- code/agent_MLP.py
+++ code/agent_MLP.py
@@ -3,14 +3,14 @@ import numpy as np
 N = 20 # nubmer of neurons
 Mes = 3 # number of messages
 Mass = 40 # mass of each message's food
-Tent = 5 # max number of each neuron's tentacle 
+Tent = 6 # max number of each neuron's tentacle 
 
-food = np.random.choice([-1,1],(Mes,Mass)) # A = 1,B = -1
-food = np.ones((Mes,Mass))
-food[:,:16] = -1
-food_p = (food.reshape(Mes * Mass))[:Tent*N].reshape((N,Tent))
 
-def eat():
+def io2food(xi):
+    food = np.outer(xi, np.random.choice([-1,1],Mass)) # A = 1, B= -1
+    return food
+
+def eat(food_p):
     portion = np.zeros((N,Tent))
     portion = food_p 
     np.random.shuffle(portion)
@@ -21,9 +21,12 @@ def eat():
     res = (res>0)*1
     return np.sum(res)>N/2
 
-
 if __name__ == '__main__':
+    xi = np.array([0,0,1])
+    food = io2food(xi)
+    food_p = (food.reshape(Mes * Mass))[:Tent*N].reshape((N,Tent))
+    print food_p
     num = 0
     for x in range(100):
-        num += eat() 
+        num += eat(food_p) 
     print num
